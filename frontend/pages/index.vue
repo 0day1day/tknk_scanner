5be036df9e3c463a1dac3446525b1e94935348ea
nomuken <nomuken@spica.bz>
2018-09-02 11:55:27 +0900

    :musical_score: Initial Commit at Magical Mirai 2018

---
+++ frontend/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="container">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <h1>palloc</h1>
+  </div>
+  <div class="navbar-menu">
+    <a class="navbar-link">
+    Docs
+  </a>
+  </div>
+</nav>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style lang="stylus">
+div
+  background-color gray
+</style>
+
