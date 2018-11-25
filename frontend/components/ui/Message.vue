c32e453ec255ad84e18281f2eacf22259c976eb8
nomuken <nomuken@spica.bz>
2018-11-25 16:19:56 +0900

    :sparkles: implements ui components

---
+++ frontend/components/ui/Message.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="message-container">
+    <div id="message-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Message"
+  }
+</script>
+
+<style lang="stylus" scoped>
+  #message-container
+    display flex
+    justify-content center
+    align-items center
+  #message-content
+    width 100%
+    height 100%
+
+</style>
