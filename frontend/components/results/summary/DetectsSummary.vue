a2062b2309a2cd7333212f0aafa84409ab79a7f3
nomuken <nomuken@spica.bz>
2018-11-28 04:34:36 +0900

    :construction: WIP: implements DetectsSummary

---
+++ frontend/components/results/summary/DetectsSummary.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-table :items="detectsSummary" class="summary-table" stacked fixed small></b-table>
+</template>
+
+<script>
+  export default {
+    name: "DetectsSummary",
+    props: [
+      'detectsSummary'
+    ]
+  }
+</script>
+
+<style scoped>
+
+</style>
